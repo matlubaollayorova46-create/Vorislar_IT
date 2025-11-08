c14ea345d041456db6a09b12774747e44d6cd5cd
Humoyun009 <otaxonovmadad25@gmail.com>
2025-11-08 15:24:21 +0500

    darssim

---
+++ python_takrorlash.py
@@ -0,0 +1,16 @@
+#masalalar
+# k va n sonlar berilgan (n>0).k sonini n marta chiqaruvchi programma tuzilsin.
+# print("n>0") 
+
+# k=int(input("k sonini kriting:")) 
+# n=int(input("k sonini kriting(n>0):"))
+# for i in range (n):
+#    print (k) 
+#    #a va b sonlar berilgan(a<b).a va b orasidagi barcha butun sonlar(a va b ni ham ) chiqaruvchi sonlar sonini chiqaruvchi programma tuzilsin .
+#    # (a va b ham chiqsin)k=int(input("k sonini kriting:")) 
+a=int(input("a sonini kriting:")) 
+b=int(input("b sonini kriting(a<b):")) 
+for i in range():
+  print()
+
+                   
