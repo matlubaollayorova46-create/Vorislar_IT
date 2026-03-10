08761ea9daec844d98ee88688e57dba3b0ed9b95
Matluba Ollayorova <matlubaollayorova46@gmail.com>
2026-03-10 15:04:31 +0300

    cv
    vvbvf

--- 21-dars.py
+++ 21-dars.py
@@ -34,3 +34,29 @@ def myfunction(nam):
   return "Hello " + nam
 
 print(myfunction("John"))
+def changecase(func):
+  def myinner():
+    return func().upper()
+  return myinner
+
+def addgreeting(func):
+  def myinner():
+    return "Hello " + func() + " Have a good day!"
+  return myinner
+
+@changecase
+@addgreeting
+def myfunction():
+  return "Tobias"
+
+print(myfunction())
+def changecase(func):
+  def myinner():
+    return func().upper()
+  return myinner
+
+@changecase
+def myfunction():
+  return "Have a great day!"
+
+print(myfunction.__name__)
