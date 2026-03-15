57d3dd0cf2f21d489b550f8e00f6137a28326072
Matluba Ollayorova <matlubaollayorova46@gmail.com>
2026-03-15 21:31:58 +0500

    uu
    uu

---
+++ 25-dars.py
@@ -0,0 +1,49 @@
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict["brand"])
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964,
+  "year": 2020
+}
+print(thisdict)
+thisdict = {
+  "brand": "Ford",
+  "electric": False,
+  "year": 1964,
+  "colors": ["red", "white", "blue"]
+}
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(type(thisdict))
+thisdict = dict(name = "John", age = 36, country = "Norway")
+print(thisdict)
+thisset = {"apple", "banana", "cherry"}
+print(thisset)
+thisset = {"apple", "banana", "cherry", "apple"}
+
+print(thisset)
+thisset = {"apple", "banana", "cherry", True, 1, 2}
+
+print(thisset)
+thisset = {"apple", "banana", "cherry", False, True, 0}
+
+print(thisset)
