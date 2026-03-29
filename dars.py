3d3a737b6eebb69d8021116c5e90697a15aa0d1c
Matluba Ollayorova <matlubaollayorova46@gmail.com>
2026-03-29 15:45:33 +0300

    п
    п

--- dars.py
+++ dars.py
@@ -1,12 +1,2 @@
-print("Hello, World!")
-ism=input("Ismingizni kiriting: ")
-yosh=int(input("Yoshingizni kiriting: "))   
-if yosh<18:
-    print(f"Salom {ism}, siz hali yosh ekansiz!")
-elif yosh==18:
-    print(f"Salom {ism}, siz endi kattalar qatoriga qo'shildingiz!")
-else:
-    print(f"Salom {ism}, siz kattasiz!")
-    print("Kattalar uchun maxsus takliflarimiz bor!")
-    ism=input("Ismingizni kiriting: ")
-yosh=int(input("Yoshingizni kiriting: "))
+ism=("jjkkk")
+print=(ism)
