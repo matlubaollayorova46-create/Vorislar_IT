411f51930fc2043e431cea42485e0ebac4792d03
Matluba Ollayorova <matlubaollayorova46@gmail.com>
2026-04-01 10:20:08 +0500

    h
    h

--- 28-dars.py
+++ 28-dars.py
@@ -1,59 +1,44 @@
-import numpy as np
-import matplotlib.pyplot as plt
+a="ism"
+print(a)
+a = 200
+b = 33
 
-x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
-y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
 
-plt.title("Sports Watch Data")
-plt.xlabel("Average Pulse")
-plt.ylabel("Calorie Burnage")
 
-plt.plot(x, y)
+x = "Hello"
+y = 15
 
-plt.grid()
+print(bool(x))
+print(bool(y))
 
-plt.show()
-import matplotlib.pyplot as plt
-import numpy as np
+"Ko'pgina qiymatlar haqiqatdir
+TrueDeyarli har qanday qiymat , agar u biron bir tarkibga ega bo'lsa, baholanadi .
 
-x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
-y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
+Bo'sh satrlardan tashqari istalgan satr bo'ladi True.
 
-plt.scatter(x, y)
-plt.show()
-import matplotlib.pyplot as plt
-import numpy as np
+Har qanday son True, dan tashqari, bo'ladi 0.
 
-#day one, the age and speed of 13 cars:
-x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
-y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
-plt.scatter(x, y)
+Bo'sh bo'lganlardan tashqari har qanday ro'yxat, tuple, to'plam va lug'at ga teng True.
 
-#day two, the age and speed of 15 cars:
-x = np.array([2,2,8,1,15,8,12,9,7,3,11,4,7,14,12])
-y = np.array([100,105,84,105,90,99,90,95,94,100,79,112,91,80,85])
-plt.scatter(x, y)
+Misol
+Quyidagilar True qiymatini qaytaradi:
 
-plt.show()
-import matplotlib.pyplot as plt
-import numpy as np
+bool("abc")
+bool(123)
+bool(["apple", "cherry", "banana"])
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({})
 
-x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
-y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
-plt.scatter(x, y, color = 'hotpink')
+def myFunction() :
+  return True
 
-x = np.array([2,2,8,1,15,8,12,9,7,3,11,4,7,14,12])
-y = np.array([100,105,84,105,90,99,90,95,94,100,79,112,91,80,85])
-plt.scatter(x, y, color = '#88c999')
-
-plt.show()
-import matplotlib.pyplot as plt
-import numpy as np
-
-x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
-y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
-colors = np.array(["red","green","blue","yellow","pink","black","orange","purple","beige","brown","gray","cyan","magenta"])
-
-plt.scatter(x, y, c=colors)
-
-plt.show()
+print(myFunction())
